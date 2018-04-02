2dbf973fb29bc9793f525ae14d27086828b3e710
Jungwon Seo <wsbtgt@gmail.com>
2018-04-02 23:42:19 +0200

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,20 +2,33 @@ from mrjob.job import MRJob
 from bs4 import BeautifulSoup
 import os
 import re
+import json
+class MRLinkExtractor(MRJob):
 
-class MRWordFreqCount(MRJob):
+    def mapper_init(self):
+       self.from_page = ""
+       self.in_links = False
+       self.outgoing_pages = []
 
     def mapper(self, _, line):
-        if '.html' in line and '<!DOCTYPE html' in line:
-            if 'href=' in line and '.html' in line:
-                soup = BeautifulSoup(line,"html.parser")
-                for a in soup.find_all('a', href=True):
-                    yield a['href'].split("/")[-1], 1
-                    break
+        line = line.strip()
+        if '.html' in line and '/articles/' in line and '<!DOCTYPE html' in line and not self.in_links:
+            self.in_links = True
+            self.from_page = "FROM:"+line[line.find("articles"):line.find(".html")+5].split("/")[-1]
 
-    def reducer(self, word, counts):
-        yield word, sum(counts)
+
+        if '</html>' in line and self.in_links:
+            yield self.from_page+"/"+str(len(self.outgoing_pages)), '/t'.join(self.outgoing_pages)
+            self.from_page = ""
+            self.in_links = False
+            self.outgoing_pages = []
+
+        if 'href=' in line and '.html' in line and self.in_links:
+            soup = BeautifulSoup(line,"html.parser")
+            for a in soup.find_all('a', href=True):
+                link =  a['href'].split("/")[-1]
+                self.outgoing_pages.append(link)
 
 
 if __name__ == '__main__':
-    MRWordFreqCount.run()
+    MRLinkExtractor.run()
